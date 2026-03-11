6dc5d1442a84ccc662a7982a22db264120bbe4a9
mgurga <motiurga@gmail.com>
2026-03-10 19:10:52 -0700

    implement backward elimination search

--- src/FeatureSearch.hs
+++ src/FeatureSearch.hs
@@ -1,22 +1,42 @@
-module FeatureSearch (start_feature_search, cross_validation_accuracy, ndist) where
+module FeatureSearch (
+    start_forward_feature_search,
+    start_backward_feature_search,
+    cross_validation_accuracy,
+    ndist
+) where
 
 import DataSet (Entry(..))
 
 data SearchResults = SearchResults {
     best_accuracy :: Float,
     accuracys :: [Float],
-    selected_features :: [Int]
+    selected_features :: [Int],
+    to_search :: [Int] -- only used for backward search
 } deriving (Show, Eq)
 
-start_feature_search :: [Entry] -> IO SearchResults
-start_feature_search ds = do
+start_forward_feature_search :: [Entry] -> IO SearchResults
+start_forward_feature_search ds = do
     -- create empty results to add to
-    let emptyresults = SearchResults {best_accuracy = 0, accuracys = [], selected_features = []}
-    fsr <- feature_search ds emptyresults
+    let emptyresults = 
+         SearchResults {best_accuracy = 0, accuracys = [], selected_features = [], to_search = []}
+    fsr <- forward_feature_search ds emptyresults
     return fsr
 
-feature_search :: [Entry] -> SearchResults -> IO SearchResults
-feature_search ds prev_results = do
+start_backward_feature_search :: [Entry] -> IO SearchResults
+start_backward_feature_search ds = do
+    -- start by getting accuracy of all features enabled
+    let allacc = cross_validation_accuracy ds [1..(length (features (ds!!0)))]
+    let emptyresults = SearchResults {
+        best_accuracy = allacc,
+        accuracys = [(allacc)],
+        selected_features = [],
+        to_search = [1..(length (features (ds!!0)))]
+    }
+    fsr <- backward_feature_search ds emptyresults
+    return fsr
+
+forward_feature_search :: [Entry] -> SearchResults -> IO SearchResults
+forward_feature_search ds prev_results = do
     putStrLn ("searching " ++ (show (selected_features prev_results)))
     let prev_features = (selected_features prev_results)
     -- 1 indexed feature list
@@ -34,20 +54,68 @@ feature_search ds prev_results = do
             return SearchResults {
                 best_accuracy = max (best_accuracy prev_results) acc,
                 accuracys = (accuracys prev_results) ++ [acc],
-                selected_features = new_feature_list
+                selected_features = new_feature_list,
+                to_search = []
             }
         )
         features_to_search
     -- putStrLn ("finished searching feature list " ++ (show (selected_features prev_results)))
     let best_path = best_result all_paths
     putStrLn ("\n- best accuracy " ++ (show (best_accuracy best_path)))
-    putStrLn ((show best_path))
 
     -- only call feature search on next layer if still features to test
-    if (length features_to_search) == 1 then
-        return (all_paths!!0)
+    if (length features_to_search) == 1 then do
+        let feat_acc = reverse (zip (selected_features best_path) (accuracys best_path))
+        feature_accuracy_table feat_acc
+        return best_path
     else
-        feature_search ds best_path
+        forward_feature_search ds best_path
+
+backward_feature_search :: [Entry] -> SearchResults -> IO SearchResults
+backward_feature_search ds prev_results = do
+    putStrLn ("searching " ++ (show (selected_features prev_results)))
+    -- 1 indexed feature list
+    -- start will full feature list and recursivly remove the worst features
+    let features_to_search = (to_search prev_results)
+
+    putStr ("- considering feature ")
+    all_paths <-
+        mapM
+        (\i -> do
+            putStr ((show i) ++ " ... ")
+            -- remove i from feature list
+            let new_feature_list =
+                    filter (\f -> (not (f == i))) (to_search prev_results)
+            let acc = cross_validation_accuracy ds new_feature_list
+
+            return SearchResults {
+                best_accuracy = max acc (best_accuracy prev_results),
+                accuracys =
+                    -- avoid adding empty feature list accuracy to final accuracy list
+                    (if new_feature_list /= [] then
+                        (accuracys prev_results) ++ [acc]
+                    else
+                        (accuracys prev_results)),
+                selected_features = (selected_features prev_results) ++ [i],
+                to_search = new_feature_list
+            }
+        )
+        features_to_search
+    -- putStrLn ("finished searching feature list " ++ (show (selected_features prev_results)))
+    let best_path = best_result all_paths
+    putStrLn ("\n- accuracy list " ++ (show (accuracys best_path)))
+
+    -- find removed feature
+    let feature_difference =
+            filter (\fi -> not (fi `elem` (to_search best_path))) features_to_search
+    putStrLn ("- removing feature " ++ (show (feature_difference!!0)))
+
+    -- only call feature search on next layer if still features to test
+    if (length features_to_search) == 1 then do
+        feature_accuracy_table (zip (selected_features best_path) (accuracys best_path))
+        return best_path
+    else
+        backward_feature_search ds best_path
 
 cross_validation_accuracy :: [Entry] -> [Int] -> Float
 cross_validation_accuracy _ [] = 0.5 -- if feature list is empty default accuracy is 0.5
@@ -99,3 +167,10 @@ ndist list1 list2 = do
                 list2
     let distsum = sum innerlist
     sqrt distsum
+
+feature_accuracy_table :: [(Int, Float)] -> IO ()
+feature_accuracy_table feat_acc = do
+    let featrow = foldr (\(f, _) acc -> acc ++ ((show f) ++ "\t")) "" feat_acc
+    let accrow = foldr (\(_, a) acc -> acc ++ ((take 5 (show a)) ++ "\t")) "" feat_acc
+    putStrLn ("\n" ++ featrow ++ "\n" ++ accrow ++ "\n")
+    return ()
--- src/Main.hs
+++ src/Main.hs
@@ -4,20 +4,35 @@ import DataSet (parse_text)
 
 import System.Environment (getArgs)
 import System.Exit (die)
-import FeatureSearch (start_feature_search)
+import FeatureSearch (start_forward_feature_search, start_backward_feature_search)
 
 main :: IO ()
 main = do
     args <- getArgs
 
-    if length args == 1 then
+    if length args == 1 then do
         putStrLn ("reading dataset " ++ args!!0)
+
+        dstext <- readFile (args!!0)
+        let ds = parse_text dstext
+        putStrLn ("header " ++ (show (ds!!0)))
+
+        res <- start_forward_feature_search ds
+        putStrLn ("final results " ++ (show res))
+    else if length args == 2 then do
+        putStrLn ("reading dataset " ++ args!!1)
+
+        dstext <- readFile (args!!1)
+        let ds = parse_text dstext
+        putStrLn ("header " ++ (show (ds!!0)))
+
+        if (args!!0) == "forward" then do
+            res <- start_forward_feature_search ds
+            putStrLn ("final results " ++ (show res))
+        else if (args!!0) == "backward" then do
+            res <- start_backward_feature_search ds
+            putStrLn ("final results " ++ (show res))
+        else
+            die ("invalid search option '" ++ (args!!0) ++ "' valid options are 'forward' and 'backward")
     else
         die "incorrect number of arguments, must provide dataset"
-
-    dstext <- readFile (args!!0)
-    let ds = parse_text dstext
-    putStrLn ("header " ++ (show (ds!!0)))
-
-    res <- start_feature_search ds
-    putStrLn ("final results " ++ (show res))
